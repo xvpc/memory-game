fe7f153662ea2ac24ae76260054a9684b989a6ad
Viper <viperxx100@gmail.com>
2023-04-14 05:34:56 +0300

    #

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
-import '@/styles/globals.css'
+import '@/styles/style.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/scss/style.scss'
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
